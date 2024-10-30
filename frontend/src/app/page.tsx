4c5ded8be2b598f538d7345eb47e55758ace73a3
Brandon Roberts <brandonjroberts22@gmail.com>
2024-10-30 14:27:04 -0500

    Connector as any

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -113,7 +113,6 @@ import { Chain, sepolia } from "@starknet-react/chains";
 import { StarknetConfig, starkscan } from "@starknet-react/core";
 import { RpcProvider } from "starknet";
 import ControllerConnector from "@cartridge/connector";
-import Connector from "@starknet-react/core";
 
 const ETH_TOKEN_ADDRESS =
   "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
@@ -173,7 +172,7 @@ const App = () => {
   }, [address, connector]);
 
   const doConnect = () => {
-    const conn = connector as unknown as Connector.Connector;
+    const conn = connector as any;
     connect({ connector: conn });
   }
 
